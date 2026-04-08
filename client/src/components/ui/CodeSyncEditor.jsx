5c1d90edc1d711ec99d304edcfeb19dfcdc1ab44
Vinoy Kumar <v685958@gmail.com>
2026-04-07 22:36:37 -0700

    Create mechanism to trap code content and create a simple console component

--- client/src/components/ui/CodeSyncEditor.jsx
+++ client/src/components/ui/CodeSyncEditor.jsx
@@ -2,12 +2,16 @@ import React, { useState, useRef, useEffect } from "react";
 
 import * as monaco from "monaco-editor";
 
-const Editor = () => {
+const Editor = ({consoleOn}) => {
     const code_editor = useRef(null);
 
-    const [codeLines, setCodeLines] = useState([]);
+    var codeEditor = null;
 
-    var jsCode = [
+    const [outConsole, setConsole] = useState(true)
+    const [position, setPosition] = useState({lang: "", lineNum: 0, col: 0})
+
+    const [codeLines, setCodeLines] = useState(
+        [
         "function Hello() {",
         "   console.log('Hello world');",
         "   }",
@@ -19,7 +23,10 @@ const Editor = () => {
         "Person.prototype.getAge = function () {",
         "	return this.age;",
         "};",
-    ].join("\n");
+    ].join("\n")
+    )
+
+
 
 
     useEffect(() => {
@@ -31,9 +38,9 @@ const Editor = () => {
                 colors: {
                     "editor.background": "#000000",
                 },
-            });
-            var codeEditor = monaco.editor.create(code_editor.current, {
-                value: jsCode,
+            }); 
+            codeEditor = monaco.editor.create(code_editor.current, {
+                value: codeLines,
                 language: "javascript",
                 automaticLayout: true,
                 theme: "pure-black",
@@ -44,11 +51,28 @@ const Editor = () => {
                 },
             });
 
-            console.log(codeEditor.getScrollTop());
+            
+
+            //Update the position display for the first time
+            setPosition({lang: codeEditor.getModel().getLanguageId(), lineNum: codeEditor.getPosition().lineNumber, col: codeEditor.getPosition().column})
+            //Update it after code changes
+            codeEditor.onMouseUp(()=>{
+                setPosition({ lang: codeEditor.getModel().getLanguageId(), lineNum: codeEditor.getPosition().lineNumber, col: codeEditor.getPosition().column})
+            })
+
+            var intervalID = setInterval(()=>{
+                console.log(codeEditor.getValue());
+               
+                setCodeLines(codeEditor.getValue());
+                
+            }, 10000)
 
             return () => {
                 if (codeEditor) {
+
                     codeEditor.dispose();
+                    clearInterval(intervalID);
+
                 }
             };
         }
@@ -79,17 +103,29 @@ const Editor = () => {
                     <div className="editor-info">
                         <span>UTF-8</span>
                         <span>•</span>
-                        <span>React</span>
+                        <span>{position.lang.length >= 2?`${position.lang[0].toUpperCase()}${position.lang.substring(1, position.lang.length)}`:position.lang }</span>
                         <span>•</span>
-                        <span>Ln 12, Col 24</span>
+                        <span>Ln {position.lineNum}, Col {position.col}</span>
                     </div>
                 </div>
 
-                <div
-                    ref={code_editor}
-                    className="code-editor"
-                    style={{ height: "100%" }}
-                ></div>
+
+                <div className="editor-console-cont">
+                    <div
+                        ref={code_editor}
+                        className="code-editor"
+                        style={{ height: "100%" }}
+                    ></div>
+                    {outConsole &&
+                    
+                        <div className={outConsole?"console":"console hidden"}>
+                            <div>
+                                <button className="tab-close">×</button>
+                            </div>
+                            
+                        </div>
+                    }
+                </div>
 
                 {/* Mini Map */}
                 {/* <div className="minimap">
