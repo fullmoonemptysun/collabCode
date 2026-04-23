1675f967e7192b31144cfa4468faa3e88ecc6a62
Vinoy Kumar <v685958@gmail.com>
2026-04-22 22:43:39 -0700

    Build the frontend and integrate wit server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(cors())
 app.use(morgan('tiny'))
 
